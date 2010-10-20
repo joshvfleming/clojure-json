6d7f28e58a2cd81a0e26a6c966d75b6310b638ad
Dan Larkin <dan@danlarkin.org>
2010-10-20 10:48:59 -0400

    add lein-multi plugin to test against multiple versions of clojure

--- project.clj
+++ project.clj
@@ -1,2 +1,6 @@
 (defproject org.danlarkin/clojure-json "1.2-SNAPSHOT"
-  :dev-dependencies [[org.clojure/clojure "1.2.0"]])
+  :dev-dependencies [[org.clojure/clojure "[1.1,1.2]"]
+                     [swank-clojure "1.2.1"]
+                     [lein-multi "1.0.0"]]
+  :multi-deps {"1.1" [[org.clojure/clojure "1.1.0"]]
+               "1.2" [[org.clojure/clojure "1.2.0"]]})
